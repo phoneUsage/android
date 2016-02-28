39645c2e2fd4b7ef039094bd6fa610da788b715c
bribovich <bribovich@gmail.com>
2016-02-28 15:23:07 -0500

    changed gradle id and made settings

--- app/src/main/java/edu/dartmouth/phoneusage/controllers/SettingsFragment.java
+++ app/src/main/java/edu/dartmouth/phoneusage/controllers/SettingsFragment.java
@@ -1,23 +1,18 @@
 package edu.dartmouth.phoneusage.controllers;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import android.preference.PreferenceFragment;
 import edu.dartmouth.phoneusage.R;
 
 /**
  * Created by benribovich on 2/27/16.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings,container,false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settingsfragment);
     }
-
-
 }
