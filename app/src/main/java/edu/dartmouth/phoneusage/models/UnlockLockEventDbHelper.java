58717886b3180498ef899c12edfc5cb083ad23ec
Sujay Busam <sujay.busam@gmail.com>
2016-03-02 10:12:14 -0500

    Implement UnlockLockEvent db helper

---
+++ app/src/main/java/edu/dartmouth/phoneusage/models/UnlockLockEventDbHelper.java
@@ -0,0 +1,60 @@
+package edu.dartmouth.phoneusage.models;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by SujayBusam on 3/1/16.
+ *
+ * DB helper for the UnlockLockEvent model.
+ *
+ * Do not interface with this class directly! Use the UnlockLockEventDataSource, which is a wrapper
+ * around this.
+ */
+public class UnlockLockEventDbHelper extends SQLiteOpenHelper {
+	// Singleton instance
+	private static UnlockLockEventDbHelper mInstance;
+
+	public static final String DATABASE_NAME = "unlock_lock_event.db";
+	public static final int DATABASE_VERSION = 1;
+
+	public static final String TABLE_NAME = "unlock_lock_event";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_START_TIME_MS = "start_time_ms";
+	public static final String COLUMN_END_TIME_MS = "end_time_ms";
+
+	public static final String[] ALL_COLUMNS = {
+			COLUMN_ID,
+			COLUMN_START_TIME_MS,
+			COLUMN_END_TIME_MS
+	};
+
+	// SQL statement for the initial database creation.
+	private static final String DATABASE_CREATE_SQL =
+			"CREATE TABLE IF NOT EXISTS " + TABLE_NAME
+					+ " ( " + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ COLUMN_START_TIME_MS + " DATETIME NOT NULL, "
+					+ COLUMN_END_TIME_MS + " DATETIME NOT NULL "
+					+ ");";
+
+
+	/**
+	 * Constructor should be private to prevent direct instantiation.
+	 * Make call to static method "getInstance()" instead.
+	 */
+	private UnlockLockEventDbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// Execute SQL command on this new database to create our UnlockLockEvent table.
+		db.execSQL(DATABASE_CREATE_SQL);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Nothing yet
+	}
+}
