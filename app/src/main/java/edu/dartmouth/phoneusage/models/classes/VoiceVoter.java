7f64e94c47f6150066565538e6830249eea4952a
bribovich <bribovich@gmail.com>
2016-03-04 11:11:29 -0500

    still speaking too much

---
+++ app/src/main/java/edu/dartmouth/phoneusage/models/classes/VoiceVoter.java
@@ -0,0 +1,38 @@
+package edu.dartmouth.phoneusage.models.classes;
+
+/**
+ * Created by benribovich on 3/4/16.
+ */
+public class VoiceVoter {
+
+    private long[] vote;
+    public VoiceVoter(){
+        vote = new long[2]; //noise =0, speech=1
+    }
+
+    public void incrementNoise(){
+        vote[0]++;
+    }
+
+    public void incrementSpeech(){
+        vote[1]++;
+    }
+
+    public int pollVoter(){
+        if(vote[1] > 2*vote[0]){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    public void reset(){
+        vote[0]=0;
+        vote[1]=0;
+    }
+
+
+
+}
