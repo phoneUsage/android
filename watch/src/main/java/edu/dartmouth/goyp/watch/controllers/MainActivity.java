c52429979677b5c7966e13fa3a1771a191da3ddc
Sujay Busam <sujay.busam@gmail.com>
2016-03-05 01:57:54 -0500

    Watch updates in real time! Added broadcast receiver

--- watch/src/main/java/edu/dartmouth/goyp/watch/controllers/MainActivity.java
+++ watch/src/main/java/edu/dartmouth/goyp/watch/controllers/MainActivity.java
@@ -1,35 +1,31 @@
 package edu.dartmouth.goyp.watch.controllers;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.DataMapItem;
-import com.google.android.gms.wearable.Wearable;
 
 import java.util.concurrent.TimeUnit;
 
 import edu.dartmouth.goyp.watch.R;
 
-public class MainActivity extends Activity implements
-		DataApi.DataListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends Activity {
 
 	private static final String TAG = "SVB-WatchMain";
 	private static final String UNLOCKS_KEY = "phoneusage.key.unlocks";
 	private static final String USAGE_KEY = "phoneusage.key.usage";
 
-	private GoogleApiClient mGoogleApiClient;
+	public static final String DATA_UPDATE_ACTION = "data_update_action";
+
+	private DataUpdateReceiver mDataUpdateReceiver;
+
 	private TextView mUnlocksText;
 	private TextView mUsageText;
 
@@ -45,69 +41,60 @@ public class MainActivity extends Activity implements
 			public void onLayoutInflated(WatchViewStub watchViewStub) {
 				mUnlocksText = (TextView) stub.findViewById(R.id.unlocks_text);
 				mUsageText = (TextView) stub.findViewById(R.id.usage_text);
+				updateUI();
 			}
 		});
-
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
-				.addApi(Wearable.API)
-				.addConnectionCallbacks(this)
-				.addOnConnectionFailedListener(this)
-				.build();
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		mGoogleApiClient.connect();
 	}
 
 	@Override
-	public void onConnected(Bundle connectionHint) {
-		if (Log.isLoggable(TAG, Log.DEBUG)) {
-			Log.d(TAG, "Connected to Google Api Service");
-		}
-		Wearable.DataApi.addListener(mGoogleApiClient, this);
-	}
-
-	@Override
-	public void onConnectionSuspended(int i) {
-		Log.d(TAG, "onConnectionSuspended");
-	}
-
-	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
-		Log.d(TAG, "onConnectionFailed. Result: " + connectionResult);
+	protected void onResume() {
+		Log.d(TAG, "onResume");
+		super.onResume();
+		initDataUpdateReceiver();
 	}
 
 	@Override
 	protected void onStop() {
-		if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
-			Wearable.DataApi.removeListener(mGoogleApiClient, this);
-			mGoogleApiClient.disconnect();
-		}
+		Log.d(TAG, "onStop");
 		super.onStop();
+		unregisterReceiver(mDataUpdateReceiver);
 	}
 
-	@Override
-	public void onDataChanged(DataEventBuffer dataEvents) {
-		for (DataEvent event : dataEvents) {
-			if (event.getType() == DataEvent.TYPE_DELETED) {
-				Log.d(TAG, "DataItem deleted: " + event.getDataItem().getUri());
-			} else if (event.getType() == DataEvent.TYPE_CHANGED) {
-				Log.d(TAG, "DataItem changed: " + event.getDataItem().getUri());
-				DataItem dataItem = event.getDataItem();
-				if (dataItem.getUri().getPath().compareTo("/watchData") == 0) {
-					DataMap dataMap = DataMapItem.fromDataItem(dataItem).getDataMap();
-//					Toast.makeText(this, "Unlocks: " + dataMap.getLong(UNLOCKS_KEY) + " Usage: " +
-//							dataMap.getLong(USAGE_KEY), Toast.LENGTH_SHORT).show();
+	// **************** BroadcastReceiver Implementation + Initialization ************************//
 
-					long unlocks = dataMap.getLong(UNLOCKS_KEY);
-					long usage = dataMap.getLong(USAGE_KEY);
-					updateUnlocks(unlocks);
-					updateUsage(usage);
-				}
-			}
+	public class DataUpdateReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			updateUI();
 		}
+	}
+
+	/**
+	 * Initialize and register the BroadcastReceiver that listens for data updates.
+	 */
+	private void initDataUpdateReceiver() {
+		Log.d(TAG, "initDataUpdateReceiver");
+		mDataUpdateReceiver = new DataUpdateReceiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(DATA_UPDATE_ACTION);
+		registerReceiver(mDataUpdateReceiver, intentFilter);
+	}
+
+	// ******************************* Private Helper Functions **********************************//
+
+	private void updateUI() {
+		Log.d(TAG, "Updating Values");
+		SharedPreferences sharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		long unlocks = sharedPreferences.getLong(UNLOCKS_KEY, 0);
+		long usage = sharedPreferences.getLong(USAGE_KEY, 0);
+		updateUnlocks(unlocks);
+		updateUsage(usage);
 	}
 
 	private void updateUnlocks(long unlocks) {
@@ -123,7 +110,7 @@ public class MainActivity extends Activity implements
 	/**
 	 * Return milliseconds converted to hours rounded to 2 decimal places.
 	 */
-	public float getHoursFromMillis(long millis) {
+	private float getHoursFromMillis(long millis) {
 		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
 		float hours = minutes / 60.0f;
 		return Math.round(hours * 100.0f) / 100.0f;
