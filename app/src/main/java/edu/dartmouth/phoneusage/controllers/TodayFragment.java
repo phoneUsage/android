e37bca592cfc30f3775363f5cb2cfdfb2834e458
Hunter Estrada <hunter.c.estrada.16@dartmouth.edu>
2016-03-06 20:28:55 -0500

    fixes bug where TodayView does not update when unlocked

--- app/src/main/java/edu/dartmouth/phoneusage/controllers/TodayFragment.java
+++ app/src/main/java/edu/dartmouth/phoneusage/controllers/TodayFragment.java
@@ -1,6 +1,10 @@
 package edu.dartmouth.phoneusage.controllers;
 
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,11 +20,27 @@ import edu.dartmouth.phoneusage.views.TodayCircleView;
  */
 public class TodayFragment extends Fragment implements UpdatableFragment {
     View mView;
+    UnlockReceiver mUnlockReceiver;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_today, container, false);
         return mView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUnlockReceiver = new UnlockReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_UPDATE_UI);
+        getActivity().registerReceiver(mUnlockReceiver, intentFilter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().unregisterReceiver(mUnlockReceiver);
     }
 
     @Override // updates today circle view stats when visible
@@ -29,4 +49,14 @@ public class TodayFragment extends Fragment implements UpdatableFragment {
             mView.findViewById(R.id.TodayCircleView).invalidate();
         }
     }
+
+    public class UnlockReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(getClass().getName(), "updated BX");
+            updateUI();
+        }
+    }
+
+    public static final String ACTION_UPDATE_UI = "update_ui";
 }
